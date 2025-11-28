79eafe1773c4013d6f570c728a53b2eb5f39d641
CodenameCypher <aditya.roy@g.bracu.ac.bd>
2025-11-28 13:44:51 +1100

    modified gfx utilities

--- frontend/src/pages/SignUpPage.jsx
+++ frontend/src/pages/SignUpPage.jsx
@@ -60,7 +60,7 @@ const SignUpPage = () => {
               className="w-12 h-12 rounded-xl bg-primary/10 flex items-center justify-center group-hover:bg-primary/20
               transition-colors"
             >
-              <MessageSquare className="w-6 h-6 text-primary" />
+              <img src="/logo.png" />
             </div>
             <h1 className="text-2xl font-bold mt-2">Create Account</h1>
             <p className="text-base-content/60">Get connected to Tether!</p>
